9d862f42fe5d25913502a04f25d286805a845599
Felipe Martínez <105995332+Felipe021284@users.noreply.github.com>
2025-02-22 05:21:49 -0600

    Add files via upload

---
+++ tablas de multiplicar.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tablas de multiplicar</title>
+    <style>
+
+    .padre{
+        background-color:blue;
+        display:flex;
+        flex-direction:row;
+        flex-wrap:wrap;
+    }
+
+    .hijo{
+        
+        background-color:pink;
+        height:100%;
+        width:250px;
+        padding:10px;
+        margin:60px;
+        font-size:40px;
+        text-transform:uppercase;
+      /*  color:white;*/
+      
+    }
+    </style>
+</head>
+<body>
+<h1>Tablas de multiplicar</h1>
+<div class="padre">
+<div class="hijo">
+<?php
+echo "tabla  del 1 <br>";
+$c=1;
+$num=1;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 2 <br>";
+$c=1;
+$num=2;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 3 <br>";
+$c=1;
+$num=3;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 4 <br>";
+$c=1;
+$num=4;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 5<br>";
+$c=1;
+$num=5;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 6 <br>";
+$c=1;
+$num=6;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 7 <br>";
+$c=1;
+$num=7;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 8 <br>";
+$c=1;
+$num=8;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+<div class="hijo">
+<?php
+echo "tabla  del 9 <br>";
+$c=1;
+$num=9;
+
+while ($c<=10){
+    echo $num." x ".$c." = ".$num*$c."<br>";
+    $c++;
+}
+
+?>
+</div>
+</div>
+</body>
+</html>
